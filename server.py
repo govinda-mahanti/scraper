46bbd56c7861cdcecda2ee227a1d05c1ddea1f1f
govinda-mahanti <gmahanti955@gmail.com>
2026-03-28 23:52:37 +0530

    add

--- server.py
+++ server.py
@@ -1,32 +1,11 @@
-from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, UTC
-import atexit
-
 from scripts.run_pipeline import run_pipeline
 
-app = Flask(__name__)
+print("\n[GitHub Action] Pipeline started at:", datetime.now(UTC))
 
-def run_job():
-    print("\n[Scheduler] Pipeline started at:", datetime.now(UTC))
-    try:
-        run_pipeline()
-        print("[Scheduler] Pipeline finished")
-    except Exception as e:
-        print("[Scheduler] Pipeline error:", e)
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(run_job, 'interval', hours=3)
-scheduler.start()
-
-# Run once at startup
-run_job()
-
-atexit.register(lambda: scheduler.shutdown())
-
-@app.route("/")
-def home():
-    return "Scheduler is running"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+try:
+    run_pipeline()
+    print("[GitHub Action] Pipeline finished successfully")
+except Exception as e:
+    print("[GitHub Action] Pipeline error:", e)
+    raise e
